38b3e7e9ece3785ad718e25b4c41b96920f4b54b
BolandJwh <96107404+BlandJwh@users.noreply.github.com>
2025-06-07 11:50:01 +0800

    Update diy.sh

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -10,7 +10,7 @@ function git_sparse_clone() {
 }
 
 # Default IP
-sed -i 's/192.168.10.1/192.168.10.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/files/bin/config_generate
 
 
 # Add packages
